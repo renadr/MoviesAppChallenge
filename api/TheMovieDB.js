eea192a30a8b40bd3d47db2c6a1465e879970b66
Adrien Lemaire <ladrien2@gmail.com>
2018-11-23 17:40:35 +0100

    Call the API and get the result when the user type something in the searchbar

---
+++ api/TheMovieDB.js
@@ -0,0 +1,30 @@
+
+export default class TheMovieDB {
+
+    constructor() {
+        this.APIkey = "2641812e3572593aac4d4628069de88e";
+        // this.request = "https://api.themoviedb.org/3/search/movie?api_key=2641812e3572593aac4d4628069de88e&language=en-US&page=1&include_adult=false&query=the"
+        this.header = "https://api.themoviedb.org/";
+        this.searchHeader = "3/search/movie?api_key=";
+        this.optionsForSearch = "&language=en-US&page=1&include_adult=false&";
+        this.query = "query=";
+
+    }
+
+    async findMovies(userInput) {
+        this.query = "query=" + userInput;
+        var request = this.header + this.searchHeader + this.APIkey + this.optionsForSearch + this.query;
+        try{
+            let response = await fetch(request);
+            responseJson = await response.json();
+        } catch(e){
+            console.error(e);
+        }
+        console.log(responseJson);
+        return responseJson;
+    }
+
+
+
+
+}
--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from '../styles/global.js';
+import TheMovieDB from '../api/TheMovieDB.js';
+
+const APIManager = new TheMovieDB();
 
 export default class SearchBox extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { textSearch: null };
+        this.state = { textSearch: null, results: null };
     }
 
     clearSearch = () => {
-        this.setState({textSearch:null})
+        this.setState({textSearch:null});
+    }
+
+    changeValueOfInput = (text) => {
+        this.setState({
+            textSearch: text,
+            results: APIManager.findMovies(this.state.textSearch)
+        });
     }
 
     render() {
         return (
             <View style={styles.searchBox}>
-                <TextInput placeholder="Search for a movie...." value={this.state.textSearch} style={styles.searchInput} onChangeText={(text)=>this.setState({textSearch:text})}/>
+                <TextInput placeholder="Search for a movie...." value={this.state.textSearch} style={styles.searchInput} onChangeText={this.changeValueOfInput}/>
                 <TouchableOpacity style={styles.searchBtn} onPress={this.clearSearch}>
                     <Image style={styles.searchBtnImg} source={require('../img/close.png')}/>
                 </TouchableOpacity>
